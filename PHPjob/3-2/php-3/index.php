6913e122436ad6b0259a8aeec7c8c7b770fe735e
yumi1452 <111632514+yumi1452@users.noreply.github.com>
2022-09-21 20:07:27 +0900

    ファイルのデータを読み書きする
    書き込み
    読み込み

---
+++ PHPjob/3-2/php-3/index.php
@@ -0,0 +1,41 @@
+<?php
+
+    $testFile = "test.txt";
+    $contents = "こんにちは！";
+    
+    if (is_writable($testFile)) {
+        
+        
+        $fp = fopen($testFile, "a");
+        
+        fwrite($fp, $contents);
+        
+        fclose($fp);
+        
+        echo "finish writing!!";
+    } else {
+        
+        echo "not writable!";
+        exit;
+    }
+    ?>
+    <?php
+    $test_file = "test2.txt";
+    
+    if (is_readable($test_file)) {
+        
+        
+        $fp = fopen($test_file, "r");
+        
+        while ($line = fgets($fp)) {
+            
+            echo $line.'<br>';
+        }
+        
+        fclose($fp);
+    } else {
+        
+        echo "not readable!";
+        exit;
+    }
+    ?>
