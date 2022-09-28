323168ef8f0ed2c0d3ea2562f73076df936558f2
yumi1452 <111632514+yumi1452@users.noreply.github.com>
2022-09-28 20:03:18 +0900

    練習問題1
    訂正済

--- PHPjob/3-2/php.1/index.php
+++ PHPjob/3-2/php.1/index.php
@@ -2,14 +2,10 @@
 <?php
 $fruits = ["りんご" => 100,"みかん" => 30,"もも" => 1000];
 $quantity = [3,5,3];
-
-function fruitprice($fruit,$value,$quantity) {
-      echo $fruit."は".$value * $quantity."円です。"."<br>";
+$i = 0;
+foreach($fruits as $fruit => $value) {
+      echo $fruit."は".$value * $quantity[$i]."円です。"."<br>";
+      $i++;
 }
-$keys = array_keys($fruits);
-      fruitprice($keys[0],$fruits["りんご"],$quantity[0]);
-      fruitprice($keys[1],$fruits["みかん"],$quantity[1]);
-      fruitprice($keys[2],$fruits["もも"],$quantity[2]);
-
 ?>
 
